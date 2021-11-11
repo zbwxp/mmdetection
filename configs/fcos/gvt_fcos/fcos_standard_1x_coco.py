08de2a3d8ecad880c4b3fe9bc3ca9efde94c4f13
zhangbowen17 <zhangbowen17@meituan.com>
2021-11-11 14:42:16 +1030

    fix bug in configs test_cfg hirachi

--- configs/fcos/gvt_fcos/fcos_standard_1x_coco.py
+++ configs/fcos/gvt_fcos/fcos_standard_1x_coco.py
@@ -21,9 +21,9 @@ model = dict(
         center_sampling=True,
         conv_bias=True,
         loss_bbox=dict(type='GIoULoss', loss_weight=1.0)
-    ),
-    # training and testing settings
-    test_cfg=dict(nms=dict(type='nms', iou_threshold=0.6)))
+    ))
+# training and testing settings
+test_cfg = dict(nms=dict(type='nms', iou_threshold=0.6))
 
 # dataset settings
 img_norm_cfg = dict(
