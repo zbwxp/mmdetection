063c1b47cc2398bfe218a287f293e11fd453f1a8
zhangbowen17 <zhangbowen17@meituan.com>
2021-11-12 15:39:16 +1030

    change dist_train

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -2,9 +2,8 @@
 
 CONFIG=$1
 WORK_DIR=$2
-GPUS=$3
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --work-dir $WORK_DIR --launcher pytorch ${@:3}
